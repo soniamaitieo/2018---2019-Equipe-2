8b65121db822284310fa3c2eb14048af2bc0d241
Madeleine <madeleine.violante@wanadoo.fr>
2018-10-27 16:30:03 +0200

    1ere matrice d'alignement avec score de BLOSUM62

--- src/align_pssm.py
+++ src/align_pssm.py
@@ -1,47 +1,45 @@
 #! /usr/bin/env python3
 import numpy as np
 
-# mt = np.zeros((len(seq_list[0]), 20), int)
-
-def read_mfasta(filename):
+def score_between_2_pssm(v1,v2):
     """
-    this function takes a mfasta file and returns sequences without gaps "-"
-    notes :
-    1. only one line for each fasta sequence
-    2. for the moment just output the sequences with the same length of query >>> needed to improve the codes
-    """
-    seq_list = []
-    with open (filename, 'r') as filin:
-        for line in filin:
-            if line[0] == ">" and line.find("73 aa") != -1:
-                seq = filin.readline().rstrip()
-                clean_seq = seq.replace("-", "")
-                seq_list.append(clean_seq)
-    return seq_list
-
-def score_between_2_pssm(pssm1,pssm2):
-    """
-    this function goal is to look at all collum in 2 pssm
-    and look at the correlation score of the 2 pssm
+    this function goal is to calculate score bewteen 2 vectors
 
     """
-    col_pssm1=[]
-    col_pssm2=[]
     score=0
-    for i in range(0,len(pssm1[1])) :
-        col_pssm1.append(pssm1[1][i])
-        col_pssm2.append(pssm2[2][i])
-        #analyse de score ici
+    m_blos=np.loadtxt("/home/madeleine/Documents/Projet_Meet-U_equipe2/data/BLOSUM62.txt")
+    for i in range(0,len(v1)) :
+        score=score+v1[i]*v2[i]*m_blos[i][i]
+    return score
 
+def dynamic_alignment (pssm1,pssm2) :
+    gap_score=-2
+    n=pssm1.shape[0]
+    m=pssm2.shape[0]
+    print(n,m)
+    align_matrix=np.zeros((n,m), dtype=float)
+    align_matrix[0,0]=0
+    for i in range(1,n) : align_matrix[i,0]=align_matrix[i-1,0]+gap_score
+    for j in range(1,m) : align_matrix[0,j]=align_matrix[0,j-1]+gap_score
 
+    for i in range(1,n) :
+        for j in range(1,m) :
+            match=align_matrix[i-1,j-1]+score_between_2_pssm(pssm1[i],pssm2[j])
+            align_matrix[i,j]=max(match,align_matrix[i-1,j]+gap_score,align_matrix[i,j-1]+gap_score)
 
+    print(align_matrix)
+def read_pssm(path) :
+    ##1ere ligne : nom du fasta
+    ##2eme ligne : sequence
+    ##a partir de la 3eme : tableau
+    with open(path,"r") as fillin :
+        name_fasta = fillin.readline()
+        seq=fillin.readline()
+        lines = (line for line in fillin)
+        FH = np.loadtxt(lines)
+    return(name_fasta,seq,FH)
 if __name__ == "__main__":
-    seq_list = read_mfasta("query.v2_mfasta")
-    mt1 = np.zeros((len(seq_list[0]), 20), int)
-    aa_column = "ACDEFGHIKLMNPQRSTVWY"
-    for seq in seq_list:
-        for pos in range(len(seq)):
-            aa_idx = aa_column.find(seq[pos])
-            mt1[pos, aa_idx] += 1
-    mt2 = np.zeros((len(seq_list[0]), 20), int)
-    score_between_2_pssm(mt1,mt2)
+    name_fasta1,seq1,PSSM1=read_pssm("/home/madeleine/Documents/Projet_Meet-U_equipe2/data/query_small.aamtx")
+    name_fasta2,seq2,PSSM2=read_pssm("/home/madeleine/Documents/Projet_Meet-U_equipe2/data/template_small.aamtx")
+    #score_between_2_pssm(PSSM1,PSSM2)
+    dynamic_alignment(PSSM1,PSSM2)
