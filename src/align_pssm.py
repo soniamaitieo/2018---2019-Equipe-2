62e08e216d7fd8dc2e164586f3ea0e7ef68ac5b9
soniamaitieo <tieo.soniamai@gmail.com>
2018-10-28 17:43:44 +0100

    modif score_between_2_pssm ET essai choix penalites de gaps/ aleatoire ... A REVOIR

--- src/align_pssm.py
+++ src/align_pssm.py
@@ -7,13 +7,14 @@ def score_between_2_pssm(v1,v2):
 
     """
     score=0
-    m_blos=np.loadtxt("/home/madeleine/Documents/Projet_Meet-U_equipe2/data/BLOSUM62.txt")
+    m_blos=np.loadtxt("/home/soniamai/Bureau/2018---2019-Equipe-2/data/BLOSUM62.txt")
+
     for i in range(0,len(v1)) :
-        score=score+v1[i]*v2[i]*m_blos[i][i]
+        for j in range(0,len(v2)):
+            score=score+v1[i]*v2[i]*m_blos[i][i]
     return score
 
-def dynamic_alignment (pssm1,pssm2) :
-    gap_score=-2
+def dynamic_alignment (pssm1,pssm2, gap_score) :
     n=pssm1.shape[0]
     m=pssm2.shape[0]
     print(n,m)
@@ -26,8 +27,8 @@ def dynamic_alignment (pssm1,pssm2) :
         for j in range(1,m) :
             match=align_matrix[i-1,j-1]+score_between_2_pssm(pssm1[i],pssm2[j])
             align_matrix[i,j]=max(match,align_matrix[i-1,j]+gap_score,align_matrix[i,j-1]+gap_score)
-
-    print(align_matrix)
+    return(align_matrix)
+    
 def read_pssm(path) :
     ##1ere ligne : nom du fasta
     ##2eme ligne : sequence
@@ -38,8 +39,45 @@ def read_pssm(path) :
         lines = (line for line in fillin)
         FH = np.loadtxt(lines)
     return(name_fasta,seq,FH)
+    
+    
+def choose_gap_op(pssm1,pssm2):
+    L=[]
+    gaps=[-1,-2,-3,-4,-5]
+    for gap in gaps :
+        L.append(dynamic_alignment (pssm1,pssm2, gap))
+    return(L)
+
+all_DA = choose_gap_op(pssm1,pssm2)
+
+
+# Import the libraries
+import matplotlib.pyplot as plt
+
+# matplotlib histogram
+plt.hist(list(all_DA[1][3]), color = 'blue', edgecolor = 'black',
+         bins = int(180/5))
+
+
+
+ 
+def score_shuffle_pssm(PSSM1,PSSM2) :
+    pssm1shuffle  = np.array(PSSM1)
+    pssm2shuffle =  np.array(PSSM2)
+    np.random.shuffle(pssm1shuffle)
+    np.random.shuffle(np.array(PSSM2))
+    random_da = dynamic_alignment(pssm1shuffle, pssm2shuffle)
+    return(random_da[PSSM1.shape[0] -1,PSSM2.shape[0]-1])
+
+def n_random_scores(n) :
+    for i in range (1,n):
+        print(score_shuffle_pssm(PSSM1,PSSM2))
+    
+    
+
+    
 if __name__ == "__main__":
-    name_fasta1,seq1,PSSM1=read_pssm("/home/madeleine/Documents/Projet_Meet-U_equipe2/data/query_small.aamtx")
-    name_fasta2,seq2,PSSM2=read_pssm("/home/madeleine/Documents/Projet_Meet-U_equipe2/data/template_small.aamtx")
+    name_fasta1,seq1,PSSM1=read_pssm("/home/soniamai/Bureau/2018---2019-Equipe-2/data/query_small.aamtx")
+    name_fasta2,seq2,PSSM2=read_pssm("/home/soniamai/Bureau/2018---2019-Equipe-2/data/template_small.aamtx")
     #score_between_2_pssm(PSSM1,PSSM2)
-    dynamic_alignment(PSSM1,PSSM2)
+    DA = dynamic_alignment(PSSM1,PSSM2, -2)
